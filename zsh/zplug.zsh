b131224f786714f9ec2a9cd01ed42c0ab1ed470f
bob1983 <bob.1983.g@gmail.com>
2017-03-16 13:02:55 +0900

    Added zplug configuration

---
+++ zsh/zplug.zsh
@@ -0,0 +1,12 @@
+setup_zplug() {
+  if [ `uname`=='Darwin' ]; then
+    export ZPLUG_HOME=/usr/local/opt/zplug
+  fi
+  source $ZPLUG_HOME/init.zsh
+}
+
+setup_zplug
+zplug 'zsh-users/zsh-completions', defer:3
+
+zplug load --verbose
+
