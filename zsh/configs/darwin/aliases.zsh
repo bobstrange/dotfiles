c8906f684734708d7c1c2c0f9a065740645ccd60
bob1983 <bob.1983.g@gmail.com>
2017-05-02 13:29:51 +0900

    Added chrome aliases

---
+++ zsh/configs/darwin/aliases.zsh
@@ -0,0 +1,3 @@
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
+alias chromium="/Applications/Chromium.app/Contents/MacOS/Chromium"
