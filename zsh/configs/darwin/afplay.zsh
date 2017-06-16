7301a8123176e8500e613751b3af057a2b3e9550
bob1983 <bob.1983.g@gmail.com>
2017-06-16 17:38:52 +0900

    Removed af_take

--- zsh/configs/darwin/afplay.zsh
+++ zsh/configs/darwin/afplay.zsh
@@ -1,5 +1,4 @@
 music_path="${HOME}/Dropbox/Radio"
-alias af_take="find ${music_path} -name \"*.mp3\" | sort -R | head -1"
 alias af_start="`af_take` | xargs afplay"
 alias af_pause="pgrep afplay | xargs kill -STOP"
 alias af_resume="pgrep afplay | xargs kill -CONT"
