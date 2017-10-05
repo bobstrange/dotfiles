2344016353a62d76729455771e5bff442cd42445
bob1983 <bob.1983.g@gmail.com>
2017-10-05 10:58:28 +0900

    Fixed zsh history setting --- - Changed history file size - Don't save duplicated history - Use extended history

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -1,4 +1,8 @@
 setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zhistory
+# History on memory
 HISTSIZE=4096
-SAVEHIST=4096
+# History on file
+SAVEHIST=65536
+setopt hist_ignore_dups
+setopt EXTENDED_HISTORY
