373d151f11cb74257287a58b1f5b18993e419bd3
bob1983 <bob.1983.g@gmail.com>
2018-01-04 18:32:05 +0900

    Added albert

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,122 +1,129 @@
- #!/bin/bash
+#!/bin/bash
 
- # Change dir name to English
- LANG=C xdg-user-dirs-gtk-update
- :
- # Upgrade dependencies
- echo "Updating dependencies"
- sudo apt-get update 1>/dev/null
- sudo apt-get upgrade -y 1>/dev/null
+# Change dir name to English
+LANG=C xdg-user-dirs-gtk-update
 
- # Install essential softwares
- echo "Installing essential softwares"
- sudo apt-get install -y \
- curl gcc direnv jq vim tig tmux git xclip rxvt-unicode-256color 1>/dev/null
+# Upgrade dependencies
+echo "Updating dependencies"
+sudo apt-get update 1>/dev/null
+sudo apt-get upgrade -y 1>/dev/null
 
- ## These are needed to build ruby
- sudo add-apt-repository ppa:ubuntu-toolchain-r/test
- sudo apt-get update 1> /dev/null
- sudo apt-get install -y \
- gcc-6 autoconf bison build-essential libssl-dev libyaml-dev \
- libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 \
- libgdbm-dev 1> /dev/null
+# Install essential softwares
+echo "Installing essential softwares"
+sudo apt-get install -y \
+curl gcc direnv jq vim tig tmux git xclip rxvt-unicode-256color 1>/dev/null
 
- echo "Done."
+## These are needed to build ruby
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update 1> /dev/null
+sudo apt-get install -y \
+gcc-6 autoconf bison build-essential libssl-dev libyaml-dev \
+libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 \
+libgdbm-dev 1> /dev/null
 
- ## gnome keychain
- sudo apt-get install libgnome-keyring-dev -y 1> /dev/null
- sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring 1>/dev/null
+echo "Done."
 
- # Install zsh
- echo "Starting to install zsh"
- sudo apt-get install -y zsh 1>/dev/null
- echo "Change your login shell to zsh"
- chsh -s $(which zsh)
- echo "Finished to install zsh"
+## gnome keychain
+sudo apt-get install libgnome-keyring-dev -y 1> /dev/null
+sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring 1>/dev/null
 
- # Install rcm
- echo "Starting to install rcm"
- wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
- echo "deb http://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list 1>/dev/null
- sudo apt-get update 1>/dev/null
- sudo apt-get install -y rcm 1>/dev/null
- echo "Finished to install rcm"
+# Install zsh
+echo "Starting to install zsh"
+sudo apt-get install -y zsh 1>/dev/null
+echo "Change your login shell to zsh"
+chsh -s $(which zsh)
+echo "Finished to install zsh"
 
- # Install zplug
- echo "Starting to install zplug"
- curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+# Install rcm
+echo "Starting to install rcm"
+wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
+echo "deb http://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list 1>/dev/null
+sudo apt-get update 1>/dev/null
+sudo apt-get install -y rcm 1>/dev/null
+echo "Finished to install rcm"
 
- echo "Finished to install zplug"
+# Install zplug
+echo "Starting to install zplug"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 
- # Install dropbox
- echo "Starting to install dropbox"
- cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
- echo "Finished to install dropbox"
+echo "Finished to install zplug"
 
- # Install xxxenv
- echo "Starting to install rbenv"
- git clone https://github.com/rbenv/rbenv.git ~/.rbenv
- echo "Starting to install ruby-build"
- mkdir -p ~/.rbenv/plugins
- git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
- echo "Finished to install rbenv and ruby-build"
+# Install dropbox
+echo "Starting to install dropbox"
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+echo "Finished to install dropbox"
 
- echo "Starting to install ndenv"
- git clone https://github.com/riywo/ndenv ~/.ndenv
- echo "Starting to install node-build"
- git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
- echo "Finished to install ndenv"
+# Install xxxenv
+echo "Starting to install rbenv"
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo "Starting to install ruby-build"
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+echo "Finished to install rbenv and ruby-build"
 
- # Install atom
- echo "Starting to install atom"
- curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
- sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
- sudo apt-get update 1>/dev/null
- sudo apt-get install atom 1>/dev/null
- echo "Finished install atom"
+echo "Starting to install ndenv"
+git clone https://github.com/riywo/ndenv ~/.ndenv
+echo "Starting to install node-build"
+git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
+echo "Finished to install ndenv"
 
- # Setup rc files
- echo "Setup rc files"
- env RCRC=$HOME/dotfiles/rcrc rcup
- echo "Done."
+# Install atom
+echo "Starting to install atom"
+curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update 1>/dev/null
+sudo apt-get install atom 1>/dev/null
+echo "Finished install atom"
 
- # Install go for ghq installation
- sudo apt-get install golang-go
+# Setup rc files
+echo "Setup rc files"
+env RCRC=$HOME/dotfiles/rcrc rcup
+echo "Done."
 
- # Install ghq
- go get github.com/motemen/ghq
+# Install go for ghq installation
+sudo apt-get install golang-go
 
- # Install mysql
- sudo apt-get install mysql-server libmysqlclient-dev
- mysql_secure_installation
+# Install ghq
+go get github.com/motemen/ghq
 
- # Install postgres
- sudo apt-get install postgresql postgresql-contrib libpq-dev
+# Install mysql
+sudo apt-get install mysql-server libmysqlclient-dev
+mysql_secure_installation
 
- # Install franz
- # Franz brakes windows system when it's crashed on my environment
- # So I'll comment out
- # wget -P /tmp https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.14/franz_5.0.0-beta.14_amd64.deb
- # sudo dpkg -i /tmp/franz_5.0.0-beta.14_amd64.deb
- # sudo apt-get install -f -y
- # sudo apt-get update
+# Install postgres
+sudo apt-get install postgresql postgresql-contrib libpq-dev
 
- # Install ibus-mozc
- sudo apt-get install ibus-mozc -y
- echo "You need to do configure some manually."
- echo "see: https://mlny.info/2016/05/ibus-skk-on-ubuntu-xenial/"
+# Install franz
+# Franz brakes windows system when it's crashed on my environment
+# So I'll comment out
+# wget -P /tmp https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.14/franz_5.0.0-beta.14_amd64.deb
+# sudo dpkg -i /tmp/franz_5.0.0-beta.14_amd64.deb
+# sudo apt-get install -f -y
+# sudo apt-get update
 
- # Install xremap
- sudo apt-get install libx11-dev -y
- git clone https://github.com/k0kubun/xremap /tmp/xremap
- cd /tmp/xremap
- make
- sudo make install
- mkdir -p ~/.config/systemd/user/
- cp -p ~/dotfiles/config/systemd/user/xremap.service ~/.config/systemd/user/xremap.service
+# Install ibus-mozc
+sudo apt-get install ibus-mozc -y
+echo "You need to do configure some manually."
+echo "see: https://mlny.info/2016/05/ibus-skk-on-ubuntu-xenial/"
 
- # Install apm packages
- apm-bundle
+# Install xremap
+sudo apt-get install libx11-dev -y
+git clone https://github.com/k0kubun/xremap /tmp/xremap
+cd /tmp/xremap
+make
+sudo make install
+mkdir -p ~/.config/systemd/user/
+cp -p ~/dotfiles/config/systemd/user/xremap.service ~/.config/systemd/user/xremap.service
 
- # Install gnome extensions
- sudo apt install -y chrome-gnome-shell
+# Install apm packages
+apm-bundle
+
+# Install gnome extensions
+sudo apt install -y chrome-gnome-shell
+
+# install albert
+wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_16.04/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
+
+echo "Installed albert but make sure you need to configure auto-restart"
