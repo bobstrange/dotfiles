2457b539c855a575f82499142e40cd5e3e1010e1
bob1983 <bob.1983.g@gmail.com>
2018-01-04 10:12:40 +0900

    Remove franz installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -94,9 +94,9 @@ mysql_secure_installation
 sudo apt-get install postgresql postgresql-contrib libpq-dev
 
 # Install franz
-wget -P /tmp https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.14/franz_5.0.0-beta.14_amd64.deb 
-sudo dpkg -i /tmp/franz_5.0.0-beta.14_amd64.deb
-sudo apt-get install -f -y
-sudo apt-get update
-sudo dpkg -i /tmp/franz_5.0.0-beta.14_amd64.deb
-
+# Franz brakes windows system when it's crashed on my environment
+# So I'll comment out
+# wget -P /tmp https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.14/franz_5.0.0-beta.14_amd64.deb 
+# sudo dpkg -i /tmp/franz_5.0.0-beta.14_amd64.deb
+# sudo apt-get install -f -y
+# sudo apt-get update
