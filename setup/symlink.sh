ecfcc5a460785572d2c39f2ac388a900543a65e4
bob1983 <bob.1983.g@gmail.com>
2018-01-09 01:33:17 +0900

    Fixed .ssh permission

--- setup/symlink.sh
+++ setup/symlink.sh
@@ -4,3 +4,11 @@ ln -s ~/Dropbox/config/.ssh ~/.ssh
 ln -s ~/Dropbox/config/github_token ~/.github_token
 ln -s ~/Dropbox/config/aws ~/.aws
 
+update_ssh_permission() {
+  tmp="$(pwd)"
+  cd ~/.ssh/
+  ls | grep -vE "known_hosts|\.pub$|config" |xargs chmod 400
+  cd ${tmp}
+}
+
+update_ssh_permission
