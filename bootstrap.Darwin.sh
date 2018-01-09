763539c8e00d36c5ad59f5e5346f37e1feef39fd
bob1983 <bob.1983.g@gmail.com>
2018-01-09 15:00:48 +0900

    Added xxenv mysql redis

--- bootstrap.Darwin.sh
+++ bootstrap.Darwin.sh
@@ -33,6 +33,50 @@ install_casks() {
   brew cask install alfred google-chrome google-japanese-ime skype iterm2 karabiner shiftit macs-fan-control dropbox coteditor
 }
 
+install_xxenv() {
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  mkdir -p ~/.rbenv/plugins
+  eval "$(rbenv init -)"
+
+  git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
+  git clone https://github.com/riywo/ndenv ~/.ndenv
+  eval "$(ndenv init -)"
+  git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
+}
+
+setup_ruby() {
+  for version in 2.3.1 2.3.3 2.5.0; do
+    rbenv install ${version}
+    rbenv global ${version}
+    gem install bundler
+  done
+  rbenv global 2.5.0
+}
+
+setup_node() {
+  for version in 8.9.4 6.9.2; do
+    ndenv install ${version}
+  done
+  ndenv global 8.9.4
+  brew install yarn --without-node
+}
+
+install_atom() {
+  brew cask install atom
+  apm install --packages-file ~/dotfiles/atom/installed_packages
+}
+
+install_mysql() {
+  brew install mysql
+  brew services start mysql
+}
+
+install_redis() {
+  brew install redis
+  brew services start redis
+}
+
 if [ $(uname) == 'Darwin' ]; then
   . setup/install_homebrew.sh
   setup_dotfile
@@ -41,6 +85,10 @@ if [ $(uname) == 'Darwin' ]; then
   install_zsh
   install_font
   change_host
+  install_xxenv
+  install_atom
+  install_mysql
+  install_redis
 else
   echo "This script doesn't support $(uname)"
   exit 1
