6b9bb6b1d8d43dc517bb685ccc5275ae68d64edf
bob1983 <bob.1983.g@gmail.com>
2018-01-18 21:21:36 +0900

    Use ag as fzf's source for respecting git ignore

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -1,5 +1,10 @@
+# Color scheme
 # molokai https://github.com/junegunn/fzf/wiki/Color-schemes#molokai
 export FZF_DEFAULT_OPTS='
 --color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81
 --color info:144,prompt:161,spinner:135,pointer:135,marker:118
 '
+
+# Setting ag as the default source for fzf
+export FZF_DEFAULT_COMMAND='ag -g ""'
+
