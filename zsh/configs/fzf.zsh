2eb34fdd1ede44162c09fc269dd378ab3b9829f9
bob1983 <bob.1983.g@gmail.com>
2018-01-18 20:40:10 +0900

    Set fzf color molokai

---
+++ zsh/configs/fzf.zsh
@@ -0,0 +1,5 @@
+# molokai https://github.com/junegunn/fzf/wiki/Color-schemes#molokai
+export FZF_DEFAULT_OPTS='
+--color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81
+--color info:144,prompt:161,spinner:135,pointer:135,marker:118
+'
