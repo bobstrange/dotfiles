9fbb5edb07ead5b3c7fca45b22e87a6e8239f269
bob1983 <bob.1983.g@gmail.com>
2018-01-18 23:30:12 +0900

    Added fzf alias for git

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -6,5 +6,53 @@ export FZF_DEFAULT_OPTS='
 '
 
 # Setting ag as the default source for fzf
-export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_DEFAULT_COMMAND='ag -g "" --hidden --smart-case'
+# To apply the command to CTRL-T and ALT-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
 
+# Git
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+## Browse commit history
+gshow() {
+  local _gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
+  local _viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy'"
+
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --no-sort --reverse --tiebreak=index --no-multi \
+      --ansi --preview $_viewGitLogLine \
+          --preview-window="right:70%" \
+          --header "enter to view, alt-y to copy hash" \
+          --bind "enter:execute:$_viewGitLogLine   | less -R" \
+            --bind "alt-y:execute:$_gitLogLineToHash | xclip"
+}
+
+# Browse branchs and checkout
+gbr() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux \
+           --reverse \
+           -d 30% +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+gco() {
+  local _gitDiff='echo {} | sed "s/^\sM\s//" | xargs git diff --color=always | diff-so-fancy'
+  local files file
+  files=$(git status --short --untracked-files=no)
+  file=$(echo "$files" |
+         fzf-tmux \
+         --reverse -d "70%" \
+         --no-multi --no-sort --tiebreak=index \
+         --ansi --preview ${_gitDiff} \
+         --preview-window="right:80%" \
+         +m) &&
+  echo "$file"
+  git checkout -- $(echo "${file}" | sed "s/^\sM\s//")
+}
