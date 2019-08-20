d59a38e890f6e7ce4fa0d64b258448b725cd3e5d
bob1983 <bob.1983.g@gmail.com>
2019-08-20 11:52:52 +0900

    Fix history

--- tag-zsh/zsh/configs/history.zsh
+++ tag-zsh/zsh/configs/history.zsh
@@ -1,4 +1,10 @@
-setopt hist_ignore_all_dups inc_append_history
+setopt hist_ignore_all_dups
+
+# Appends every command to the history file once it is executed
+setopt inc_append_history
+# Reloads the history whenever you use it
+setopt share_history
+
 HISTFILE=~/.zhistory
 # History on memory
 HISTSIZE=4096
