9e9194d6391ac8a59d851464570c415f24992ec6
bob1983 <bob.1983.g@gmail.com>
2020-01-07 23:55:37 +0900

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,19 +4,42 @@ usage() {
   echo "Usage: $0 [ubuntu|osx|wsl]"
 }
 
-if [ $# -ne 1 ]; then
-  echo $(usage)
-  exit 1
-fi
+install_ansible() {
+  if ! type pip; then
+    echo "Installing pip"
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py --user
+    rm get-pip.py
+  fi
 
-if [ $1 = 'ubuntu' ]; then
-  ansible-playbook -i ansible/hosts.ini -l local_ubuntu ansible/main.yml --ask-become-pass -vvv --check
-fi
+  if ! type ansible; then
+    echo "Installing ansible"
+    pip install --user ansible
+  fi
+}
 
-if [ $1 = 'osx' ]; then
-  ansible-playbook -i ansible/hosts.ini -l local_osx ansible/main.yml --ask-become-pass -vvv
-fi
+check_arg() {
+  if [ $1 -ne 1 ]; then
+    echo $(usage)
+    exit 1
+  fi
+}
 
-if [ $1 = 'wsl' ]; then
-  ansible-playbook -i ansible/hosts.ini -l local_wsl ansible/main.yml --ask-become-pass -vvv
-fi
+run_playbook() {
+  echo "Arg: $1"
+  if [[ $1 = 'ubuntu' ]]; then
+    ansible-playbook -i ansible/hosts.ini -l local_ubuntu ansible/main.yml --ask-become-pass -vvv
+  fi
+
+  if [[ $1 = 'osx' ]]; then
+    ansible-playbook -i ansible/hosts.ini -l local_osx ansible/main.yml --ask-become-pass -vvv
+  fi
+
+  if [[ $1 = 'wsl' ]]; then
+    ansible-playbook -i ansible/hosts.ini -l local_wsl ansible/main.yml --ask-become-pass -vvv
+  fi
+}
+
+check_arg $#
+install_ansible
+run_playbook $1
