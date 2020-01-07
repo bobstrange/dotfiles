c1789e47fde37b569c362ad9a3697c750990452b
bob1983 <bob.1983.g@gmail.com>
2020-01-07 21:27:33 +0900

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-if [[ -f ./setup/bootstrap.$(uname).sh ]]; then
-  ./setup/bootstrap.$(uname).sh
-else
-  uname -v | grep -q "Ubuntu"
-  ubuntu=$?
-  if [[ ${ubuntu} = 0 ]]; then
-    ./setup/bootstrap.Ubuntu.sh
-  else
-    echo 'Quit'
-    exit 1
-  fi
+usage() {
+  echo "Usage: $0 [ubuntu|osx|wsl]"
+}
+
+if [ $# -ne 1 ]; then
+  echo $(usage)
+  exit 1
+fi
+
+if [ $1 = 'ubuntu' ]; then
+  ansible-playbook -i ansible/hosts.ini -l local_ubuntu ansible/main.yml --ask-become-pass -vvv --check
+fi
+
+if [ $1 = 'osx' ]; then
+  ansible-playbook -i ansible/hosts.ini -l local_osx ansible/main.yml --ask-become-pass -vvv
+fi
+
+if [ $1 = 'wsl' ]; then
+  ansible-playbook -i ansible/hosts.ini -l local_wsl ansible/main.yml --ask-become-pass -vvv
 fi
