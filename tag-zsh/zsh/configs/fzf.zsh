dddc8d20954a8a0bafc665a90446a9e72eb79b3a
bob1983 <bob.1983.g@gmail.com>
2020-01-25 18:18:13 +0900

    Remove ag

--- tag-zsh/zsh/configs/fzf.zsh
+++ tag-zsh/zsh/configs/fzf.zsh
@@ -4,10 +4,6 @@ export FZF_DEFAULT_OPTS='
 --color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81
 --color info:144,prompt:161,spinner:135,pointer:135,marker:118
 '
-
-# Setting ag as the default source for fzf
-# export FZF_DEFAULT_COMMAND='ag -g "" --hidden --smart-case'
-
 # Experimental: use rg as the default source for fzf instead of ag
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 
