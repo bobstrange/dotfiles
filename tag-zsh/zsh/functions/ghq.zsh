df5a9ee2a4e22f3b8db031fc8705b0a51aa7c316
bob1983 <bob.1983.g@gmail.com>
2020-02-19 20:07:27 +0900

    ghq-look command

--- tag-zsh/zsh/functions/ghq.zsh
+++ tag-zsh/zsh/functions/ghq.zsh
@@ -1,9 +1,12 @@
-ghq() {
-  if [[ $1 == "look" ]]; then
-    local repo_path
-    repo_path=$(command ghq list --full-path --exact $2)
-    cd ${repo_path}
-  else
-    command ghq "$@"
+ghq-look() {
+  local repo=$(ghq list|fzf --query "${LBUFFER}")
+  if [ -n "${repo}" ]; then
+    repo=$(ghq list --full-path --exact ${repo})
+    BUFFER="cd ${repo}"
+    zle accept-line
   fi
+  zle clear-screen
 }
+
+zle -N ghq-look
+bindkey "^@" ghq-look # ctrl @
