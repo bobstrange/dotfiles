2963d189e7de2c790d3425f849144033565ec0f3
bobstrange <bob.1983.g@gmail.com>
2021-07-08 14:08:07 +0900

    chore: update hist file config

--- tag-zsh/zsh/configs/history.zsh
+++ tag-zsh/zsh/configs/history.zsh
@@ -1,14 +1,19 @@
 setopt hist_ignore_all_dups
+setopt hist_ignore_dups
 
 # Appends every command to the history file once it is executed
 setopt inc_append_history
 # Reloads the history whenever you use it
 setopt share_history
 
-HISTFILE=~/.zhistory
-# History on memory
-HISTSIZE=4096
-# History on file
-SAVEHIST=65536
-setopt hist_ignore_dups
+# Add timestamp to history
 setopt EXTENDED_HISTORY
+
+HISTFILE=~/.zhistory
+
+# History on memory
+HISTSIZE=65535
+# History on file
+SAVEHIST=1000000000
+
+
