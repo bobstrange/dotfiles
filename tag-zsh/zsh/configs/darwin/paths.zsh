cd6d5b09d864b9d24dc2b9278d91d7d4f03c9c70
bobstrange <bob.1983.g@gmail.com>
2022-05-09 01:36:03 +0900

    fix: fix gnu core utils path

--- tag-zsh/zsh/configs/darwin/paths.zsh
+++ tag-zsh/zsh/configs/darwin/paths.zsh
@@ -1,12 +1,12 @@
 # For gnu core-utils
-export PATH=/usr/local/opt/coreutils/libexec/gnubin:${PATH}
-export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}
+export PATH=/opt/homebrew/opt/coreutils/libexec/gnubin:${PATH}
+export MANPATH=/opt/homebrew/opt/coreutils/libexec/gnuman:${MANPATH}
 
 # Vscode
 export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
 
 # openssl
-export PATH="/usr/local/opt/openssl/bin:$PATH"
+export PATH="/opt/homebrew/opt/openssl/bin:$PATH"
 
 # asdf
 test -s "$(brew --prefix asdf)" && source "$(brew --prefix asdf)/asdf.sh"
