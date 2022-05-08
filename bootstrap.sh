237345f4c92c542f1f0a7a9fede9bca2abb595ba
bobstrange <bob.1983.g@gmail.com>
2022-05-08 16:24:20 +0900

    fix: fix ansible installation for osx

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,42 @@ usage() {
   echo "Usage: $0 [ubuntu|osx|wsl]"
 }
 
+install_homebrew() {
+  if ! ls /opt/homebrew/bin/brew; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+}
+
+install_python() {
+  if [[ $1 = 'osx' ]]; then
+    install_homebrew
+    eval $(/opt/homebrew/bin/brew shellenv)
+    brew update
+    brew install openssl readline sqlite3 xz zlib
+    brew install python
+    pip3 install --upgrade pip
+  fi
+}
+
 install_ansible() {
-  if ! type pip; then
-    echo "Installing pip"
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py --user
-    rm get-pip.py
+  install_python $1
+
+  if ! [[ $1 = 'osx' ]]; then
+    if ! type pip3; then
+      echo "Installing pip"
+      curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+      python3 get-pip.py --user
+      rm get-pip.py
+    fi
+
+    if ! type ansible; then
+      echo "Installing ansible"
+      pip3 install --user ansible
+    fi
   fi
 
-  if ! type ansible; then
-    echo "Installing ansible"
-    pip install --user ansible
+  if [[ $1 = 'osx' ]]; then
+    brew install ansible
   fi
 }
 
@@ -41,5 +66,5 @@ run_playbook() {
 }
 
 check_arg $#
-install_ansible
+install_ansible $1
 run_playbook $1
