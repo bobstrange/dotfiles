da902350f9a1ae7d6096c7356342328a764746e3
bobstrange <bob.1983.g@gmail.com>
2024-12-19 17:40:48 +0900

    fix: fix dircolors issue

--- tag-zsh/zsh/configs/color.zsh
+++ tag-zsh/zsh/configs/color.zsh
@@ -4,4 +4,11 @@ colors
 
 # enable colored output from ls, etc. on FreeBSD-based systems
 export CLICOLOR=1
-[[ -f ${HOME}/.dircolors ]] && eval $(dircolors -b ${HOME}/.dircolors)
+
+# Check for macOS and use gdircolors if available, otherwise dircolors
+if [[ "$(uname)" == "Darwin" ]]; then
+  [[ -f ${HOME}/.dircolors ]] && eval $(gdircolors -b ${HOME}/.dircolors)
+else
+  [[ -f ${HOME}/.dircolors ]] && eval $(dircolors -b ${HOME}/.dircolors)
+fi
+
