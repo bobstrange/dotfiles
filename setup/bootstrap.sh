6bdf75a3331d57c91ea09d0b25d3954b9c63a595
bobstrange <bob.1983.g@gmail.com>
2025-01-17 21:34:17 +0900

    fix: fix bootstrap script

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -5,17 +5,17 @@ usage() {
 }
 
 install_ansible() {
-  if ! type pip; then
-    echo "Installing pip"
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py --user
-    rm get-pip.py
+  if [[ $1 = 'ubuntu' || $1 = 'wsl' ]]; then
+    sudo apt update && sudo apt install pipx
+    pipx ensurepath
   fi
 
-  if ! type ansible; then
-    echo "Installing ansible"
-    pip install --user ansible
+  if [[ $1 = 'osx' ]]; then
+    brew update && brew install pipx
+    pipx ensurepath
   fi
+
+  pipx install --include-deps ansible
 }
 
 check_arg() {
@@ -28,18 +28,18 @@ check_arg() {
 run_playbook() {
   echo "Arg: $1"
   if [[ $1 = 'ubuntu' ]]; then
-    ansible-playbook -i ansible/hosts.ini -l local_ubuntu ansible/main.yml --ask-become-pass -vvv
+    ansible-playbook -i ansible/inventory/hosts.yml -l ubuntu ansible/main.yml --ask-become-pass -vvv
   fi
 
   if [[ $1 = 'osx' ]]; then
-    ansible-playbook -i ansible/hosts.ini -l local_osx ansible/main.yml --ask-become-pass -vvv
+    ansible-playbook -i ansible/inventory/hosts.yml -l osx ansible/main.yml --ask-become-pass -vvv
   fi
 
   if [[ $1 = 'wsl' ]]; then
-    ansible-playbook -i ansible/hosts.ini -l local_wsl ansible/main.yml --ask-become-pass -vvv
+    ansible-playbook -i ansible/inventory/hosts.yml -l wsl ansible/main.yml --ask-become-pass -vvv
   fi
 }
 
 check_arg $#
-install_ansible
+install_ansible $1
 run_playbook $1
