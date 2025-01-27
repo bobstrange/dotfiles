94d3a7b78e7485ff4259551f36df68bba532f12b
bobstrange <bob.1983.g@gmail.com>
2025-01-27 19:26:59 +0900

    chore: fzf-lua files and buffers keybindings

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Set leader
+vim.keymap.set("n", "<leader>p", "<cmd>lua require('fzf-lua').files()<CR>", {
+    noremap = true,
+    silent = true,
+    desc = "FZF Files"
+})
+vim.keymap.set("n", "<leader>l", "<cmd>lua require('fzf-lua').buffers()<CR>", {
+    noremap = true,
+    silent = true,
+    desc = "FZF Buffers"
+})
