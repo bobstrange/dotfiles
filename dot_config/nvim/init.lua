e22fd78cdd739d6c79b354c5a53fc6528471517f
bobstrange <bob.1983.g@gmail.com>
2025-08-25 00:05:13 +0900

    chore: disable conceal

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,3 +2,10 @@
 vim.cmd("source ~/.vim/base_config.vim")
 vim.cmd("source ~/.vim/keybindings.vim")
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.conceallevel = 0 -- backtickを表示
+	end,
+})
