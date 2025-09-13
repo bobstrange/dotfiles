5514157906393ebf7b1c596e5c11f6c2a06ca773
bobstrange <bob.1983.g@gmail.com>
2025-09-14 00:47:01 +0900

    chore: update bun completions things

--- dot_zsh/configs/completions.zsh
+++ dot_zsh/configs/completions.zsh
@@ -1,1 +1,4 @@
 command -v sheldon &>/dev/null && eval "$(sheldon completions --shell zsh)"
+command -v mise &>/dev/null && eval "$(mise completion zsh)"
+command -v bun &>/dev/null && ([[ -f "$HOME/.bun/_bun" ]] || bun completions) && source "$HOME/.bun/_bun"
+
