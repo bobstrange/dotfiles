c07972b339845ee11097432f17ddfdba859c82f6
bobstrange <bob.1983.g@gmail.com>
2025-09-17 15:32:42 +0900

    chore: disable conceal for now

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,9 +3,20 @@ vim.cmd("source ~/.vim/base_config.vim")
 vim.cmd("source ~/.vim/keybindings.vim")
 require("config.lazy")
 
+require("nvim-treesitter.configs").setup({
+  highlight = {
+    enable = true,
+    -- conceal を無効化
+    additional_vim_regex_highlighting = false,
+  },
+  -- conceal を無効化
+  incremental_selection = { enable = false },
+})
+
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.opt_local.conceallevel = 0 -- backtickを表示
-	end,
+  pattern = "*",
+  callback = function()
+    vim.opt_local.conceallevel = 0 -- backtickを表示
+    vim.opt_local.concealcursor = ""
+  end,
 })
