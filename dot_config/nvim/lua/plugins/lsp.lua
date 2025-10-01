c3061e806d21646e2cc0a17876be7ed90877a523
bobstrange <bob.1983.g@gmail.com>
2025-10-01 11:26:51 +0900

    chore: pin mason versions

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,28 +1,18 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = {
-      -- for JavaScript
-      "ts_ls",
-      "deno",
-      "prettierd",
-      -- for lua
-      "lua_ls",
-      "stylua",
-      -- for elixir
-      "expert",
-      -- for ShellScript
-      "shfmt",
-      -- for Markdown
-      "textlint",
-      "marksman",
-      -- for Postgres
-      "pgformatter",
-      "postgrestools",
+    "mason-org/mason-lspconfig.nvim",
+    version = "^1.0.0",
+    opts = {
+        ensure_installed = { -- for JavaScript
+        "ts_ls", "deno", "prettierd", -- for lua
+        "lua_ls", "stylua", -- for elixir
+        "expert", -- for ShellScript
+        "shfmt", -- for Markdown
+        "textlint", "marksman", -- for Postgres
+        "pgformatter", "postgrestools"}
     },
-  },
-  dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
-    "neovim/nvim-lspconfig",
-  },
+    dependencies = {{
+        "mason-org/mason.nvim",
+        version = "^1.0.0",
+        opts = {}
+    }, "neovim/nvim-lspconfig"}
 }
