0a124784a4e87c462a16b637b16552fc659dc1e4
bobstrange <bob.1983.g@gmail.com>
2025-10-04 21:24:44 +0900

    feat: change color github-dark

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -1,9 +1,29 @@
 return {
-  -- customize default LazyVim plugins
+  -- GitHub Dark theme
   {
-    "folke/tokyonight.nvim",
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = false,
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+            types = "italic,bold",
+          },
+        },
+      })
+      vim.cmd("colorscheme github_dark")
+    end,
+  },
+
+  -- Configure LazyVim to use GitHub Dark
+  {
+    "LazyVim/LazyVim",
     opts = {
-      style = "night",
+      colorscheme = "github_dark",
     },
   },
 
