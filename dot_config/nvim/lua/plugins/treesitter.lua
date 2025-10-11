412a52bac0b790b2446ec64c74ab86668426a31e
bobstrange <bob.1983.g@gmail.com>
2025-10-11 23:31:48 +0900

    chore: update

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,14 @@ return {
     config = function()
         -- List of parsers to support
         local parsers = {
+            -- Essential for Noice.nvim
+            "vim",
+            "regex",
+            "lua",
             "bash",
+            "markdown",
+            "markdown_inline",
+            -- User requested languages
             "diff",
             "editorconfig",
             "eex",
@@ -14,8 +21,6 @@ return {
             "html",
             "javascript",
             "json",
-            "markdown",
-            "markdown_inline",
             "ruby",
             "scss",
             "sql",
@@ -60,5 +65,24 @@ return {
                 pcall(vim.treesitter.start)
             end,
         })
+
+        -- Install essential parsers on startup for Noice.nvim
+        vim.schedule(function()
+            local essential_parsers = { "vim", "regex", "lua", "bash", "markdown", "markdown_inline" }
+            for _, parser in ipairs(essential_parsers) do
+                local ok = pcall(vim.treesitter.get_parser, 0, parser)
+                if not ok then
+                    -- Try TSInstall command (works with newer nvim-treesitter)
+                    local success = pcall(vim.cmd, "TSInstall " .. parser)
+                    if not success then
+                        -- If TSInstall doesn't exist, try using the API directly
+                        pcall(function()
+                            local configs = require("nvim-treesitter.configs")
+                            configs.commands.TSInstall.run(parser)
+                        end)
+                    end
+                end
+            end
+        end)
     end,
 }
