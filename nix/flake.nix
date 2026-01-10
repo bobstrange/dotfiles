f5a9c2866619a75d346fbe85972b8eebc83684c6
bobstrange <bob.1983.g@gmail.com>
2026-01-11 01:17:07 +0900

    feat: add flake.nix

---
+++ nix/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "bob's dotfiles - package management with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      linuxSystem = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${linuxSystem};
+    in
+    {
+      homeConfigurations = {
+        # Ubuntu configuration
+        "bob@ubuntu" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home.nix
+            {
+              home.username = "bob";
+              home.homeDirectory = "/home/bob";
+            }
+          ];
+        };
+
+        # WSL configuration (same as ubuntu for now)
+        "bob@wsl" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home.nix
+            {
+              home.username = "bob";
+              home.homeDirectory = "/home/bob";
+            }
+          ];
+        };
+      };
+
+      # Development shell for working on this flake
+      devShells.${linuxSystem}.default = pkgs.mkShell {
+        packages = [ pkgs.home-manager ];
+      };
+    };
+}
