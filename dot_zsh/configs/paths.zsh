e2e24d0135188f4c38096ef349c5e180713b20f9
bobstrange <bob.1983.g@gmail.com>
2026-01-12 00:29:28 +0900

    refactor: refactor path related

--- dot_zsh/configs/paths.zsh
+++ dot_zsh/configs/paths.zsh
@@ -1,60 +1,24 @@
-# Add xdg config home
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-
-# $HOME/bin
-export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+# Tool initializations (PATH is set in dot_zshenv)
 
 # rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
-command rbenv 2>/dev/null && eval "$(rbenv init -)"
+command -v rbenv &>/dev/null && eval "$(rbenv init -)"
 
 # nodenv
-export PATH="$HOME/.nodenv/bin:$PATH"
-command nodenv 2>/dev/null && eval "$(nodenv init -)"
-
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
+command -v nodenv &>/dev/null && eval "$(nodenv init -)"
 
 # pyenv
-export PATH="$HOME/.pyenv/bin:$PATH"
-export PYENV_ROOT="$HOME/.pyenv"
-command pyenv 2>/dev/null && eval "$(pyenv init -)"
-command virtualenv 2>/dev/null && eval "$(pyenv virtualenv-init -)"
+command -v pyenv &>/dev/null && eval "$(pyenv init -)"
+command -v pyenv &>/dev/null && eval "$(pyenv virtualenv-init -)" 2>/dev/null
 
 # golang
-export GOPATH="$HOME/dev"
-export PATH="$GOPATH/bin:$PATH"
-command go 2>/dev/null && export GOROOT=$(go env GOROOT)
-export GO111MODULE=on
+command -v go &>/dev/null && export GOROOT=$(go env GOROOT)
 
 # direnv
-eval "$(direnv hook zsh)"
-
-# tfenv
-export PATH="$HOME/.tfenv/bin:$PATH"
+command -v direnv &>/dev/null && eval "$(direnv hook zsh)"
 
 # github token
 test -s ~/.github_token && source ~/.github_token
 
-## Elixir
-# nss ( for mkcert )
-export PATH="/usr/local/opt/nss/bin:$PATH"
-
-# Rust
-export PATH="$HOME/.cargo/bin:$PATH"
-
-# Deno
-
-export DENO_INSTALL="/home/bob/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
-
-export PATH="/usr/local/bin:$PATH"
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
-
+# zsh completions path
 fpath=("$XDG_DATA_HOME/zsh/completions" $fpath)
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
 
