9206eba7dbc94c8c4f3a8b2a4dd06b0f4bd7728c
bobstrange <bob.1983.g@gmail.com>
2026-02-22 16:48:35 +0900

    chore: remove afplay related

--- dot_zsh/configs/darwin/afplay.zsh
+++
@@ -1,5 +0,0 @@
-music_path="${HOME}/Dropbox/Radio"
-alias af_pause="pgrep afplay | xargs kill -STOP"
-alias af_resume="pgrep afplay | xargs kill -CONT"
-alias af_stop="pkill afplay"
-alias af_playing="ps axu|grep [a]fplay|awk '{print $12}'"
